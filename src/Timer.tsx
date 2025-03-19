26034b5ce4785199feb8852fd85d0294a1b4b5a9
michael-antar <michaeldantar@gmail.com>
2025-03-18 22:36:14 -0700

    Added pomodoro steps

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,9 +1,25 @@
 import { useState, useEffect } from 'react';
 
-const START_TIME = 5;
+const WORK_STEP = { name: 'work', duration: 1500 };
+const SHORT_BREAK_STEP = { name: 'short break', duration: 300 };
+const LONG_BREAK_STEP = { name: 'long break', duration: 600 };
+
+const initialSteps = [
+    {...WORK_STEP, id: 1},
+    {...SHORT_BREAK_STEP, id: 2},
+    {...WORK_STEP, id: 3},
+    {...SHORT_BREAK_STEP, id: 4},
+    {...WORK_STEP, id: 5},
+    {...SHORT_BREAK_STEP, id: 6},
+    {...WORK_STEP, id: 7},
+    {...SHORT_BREAK_STEP, id: 8},
+    {...LONG_BREAK_STEP, id: 9}
+];
 
 function Timer() {
-    const [seconds, setSeconds] = useState(START_TIME);
+    const [steps, setSteps] = useState(initialSteps);
+    const [seconds, setSeconds] = useState(initialSteps[0].duration);
+
     const [isActive, setIsActive] = useState(false);
     const [isDone, setIsDone] = useState(false);
 
@@ -12,10 +28,10 @@ function Timer() {
     
         const interval = setInterval(() => {
             setSeconds((s) => {
+                // Timer done
                 if (s <= 1) {
                     clearInterval(interval);
-                    setIsActive(false);
-                    setIsDone(true);
+                    nextStep();
                     return 0;
                 }
                 return s - 1;
@@ -25,13 +41,36 @@ function Timer() {
         return () => clearInterval(interval);
     }, [isActive]);
 
+    function nextStep() {
+        setIsActive(false);
+        setSteps([...steps.slice(1)]);
+        if (steps.length > 1) {
+            setSeconds(steps[1].duration);
+        }
+        else {
+            setSeconds(0);
+            setIsDone(true);
+        }
+    }
+
     function handleStartStop() {
         setIsActive(!isActive);
     }
 
-    function handleReset() {
-        setSeconds(START_TIME);
+    function handleRedo() {
+        setSeconds(steps[0].duration);
         setIsActive(false);
+    }
+
+    function handleSkip() {
+        nextStep();
+    }
+
+    function handleRestart() {
+        setSteps(initialSteps);
+        setSeconds(initialSteps[0].duration);
+        setIsActive(false);
+
         setIsDone(false);
     }
 
@@ -43,12 +82,20 @@ function Timer() {
 
     return (
         <div>
+            <h1>{!isDone ? `Current Step: ${steps[0].name}` : "All Done"}</h1>
             <h1>Timer: {displayTime(seconds)}</h1>
-            <button onClick={handleStartStop}>
+            <button onClick={handleRedo} disabled={isDone}>Redo</button>
+            <button onClick={handleStartStop} disabled={isDone}>
                 {isActive ? "Stop" : "Start"}
             </button>
-            <button onClick={handleReset}>Reset</button>
-            <h2>{isDone ? "DONE" : ""}</h2>
+            <button onClick={handleSkip} disabled={isDone}>Skip</button>
+            <h3>Remaining Steps</h3>
+            <ol>
+                {steps.slice(1).map(step => {
+                    return <li key={step.id}>{step.name}</li>
+                })}
+            </ol>
+            <button onClick={handleRestart}>Restart</button>
         </div>
     )
 }
